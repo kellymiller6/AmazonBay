0c0ffa95dfa76ec979bcb6ebc225e7bfb3cb6323
Kelly Miller <kellyannemiller@gmail.com>
2017-07-27 14:45:28 -0600

    add order get and post test

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -67,4 +67,77 @@ describe('API Routes', () => {
       });
     });
   });
+
+  describe('GET /api/v1/orders', () => {
+    it('should return all items', (done) => {
+      chai.request(server)
+      .get('/api/v1/orders')
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.should.be.a('object');
+        response.body.length.should.equal(3);
+        done();
+      });
+    });
+
+    it('should not return all items if correct endpoint not hit', (done) => {
+      chai.request(server)
+      .get('/api/v1/order')
+      .end((error, response) => {
+        response.should.have.status(404);
+        done();
+      });
+    });
+  });
 });
+
+describe('POST /api/v1/orders', () => {
+    it('should create a new order', (done) => {
+      chai.request(server)
+      .post('/api/v1/orders')
+      .send({
+        id: 4,
+        total_price: '600'
+      })
+      .end((err, response) => {
+        response.should.have.status(201);
+        response.body.should.be.a('object');
+        response.body.should.have.property('id');
+        response.body.id.should.equal(4);
+        chai.request(server)
+        .get('/api/v1/orders/')
+        .end((err, response) => {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.be.a('array');
+          response.body.length.should.equal(4);
+          done();
+        });
+      });
+    });
+
+    it('should not create a new order', (done) => {
+      chai.request(server)
+      .post('/api/v1/orders')
+      .send({
+        id: 5,
+        total_price: ''
+      })
+      .end((err, response) => {
+        response.should.have.status(500);
+        response.body.should.be.a('object');
+        response.body.should.have.property('error');
+        response.body.error.should.equal('no order to purchase')
+        chai.request(server)
+        .get('/api/v1/orders/')
+        .end((err, response) => {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.be.a('array');
+          response.body.length.should.equal(4);
+          done();
+        });
+      });
+    });
+  });
