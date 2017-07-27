32d1fd8a707f0a7d4c03fe0820381825d73cf706
Kelly Miller <kellyannemiller@gmail.com>
2017-07-27 14:15:12 -0600

    add orders db, add post and get for orders, append orders to page

---
+++ db/migrations/20170727140400_drop-timestamp.js
@@ -0,0 +1,16 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+  	knex.schema.table('orders', (table) => {
+  		table.dropColumn('created_at');
+  		table.dropColumn('updated_at');
+  	})
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+  	knex.schema.table('orders', (table) => {
+  		table.timestamps();
+  	})
+  ])
+};
