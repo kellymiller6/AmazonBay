babb5a97d94dc20f54f50a02626b0be9483b16a3
Kelly Miller <kellyannemiller@gmail.com>
2017-07-27 11:16:53 -0600

    get cards appending to site

---
+++ public/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(() => {
+  receiveItems()
+})
+
+const receiveItems = () => {
+  fetch('/api/v1/store')
+  .then(response => response.json())
+  .then(items => {
+    if(items.length){
+      appendItems(items);
+    }else{
+      $('.cards').append(`
+        <p>No animals up for adoption at this time, please try again soon!</p>
+        `);
+      }
+    });
+  };
+
+  const appendItems = (items) => {
+    items.map(item => {
+      let displayPrice = item.price/100
+      $('.cards').append(`
+        <div class='card' data=${item.id}>
+          <h4>Title: ${item.title}</h4>
+          <h5>Description: ${item.description}</h5>
+          <img src="${item.src}" alt="${item.alt_tag}">
+          <h5>Price:$${displayPrice}</h5>
+          <button type="button" name="button">Add to Cart</button>
+        </div>
+        `)
+    })
+  }
