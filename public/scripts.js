15768dd452ac6623272fd1556bcd653ca137fce1
Kelly Miller <kellyannemiller@gmail.com>
2017-07-27 13:16:31 -0600

    add local storage to cart

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,24 @@
 $(document).ready(() => {
   receiveItems()
+  for (let i = 0; i < localStorage.length; i++){
+    const $storedItems = getStoredItems(localStorage.key(i));
+    appendStoredItems($storedItems)
+  }
 })
+
+const getStoredItems = (id) => {
+  return JSON.parse(localStorage.getItem(id));
+}
+
+const appendStoredItems = (obj) => {
+  console.log(obj);
+  $('.cart-items').append(`
+    <div class="cart-item">
+      <h5>${obj.title}</h5>
+      <h4 class='price'>${obj.price}</h4>
+    </div>`)
+  computeTotal()
+}
 
 const receiveItems = () => {
   fetch('/api/v1/store')
@@ -36,7 +54,9 @@ const appendToCart = (item) => {
   console.log(item);
   const title = item[0].innerHTML
   const price = item[3].innerHTML
-  // console.log(price.match(/\d+/)[0]);
+  const $item = {title: title, price: price}
+  const key = title
+  localStorage.setItem(title, JSON.stringify($item))
   $('.cart-items').append(`
     <div class="cart-item">
       <h5>${title}</h5>
@@ -45,6 +65,7 @@ const appendToCart = (item) => {
 }
 
 $('.cards').on('click', '.add-btn', function () {
+  console.log($(this).parent());
   const item = $(this).parent().children()
   $(this).prop('disabled', true);
   appendToCart(item)
