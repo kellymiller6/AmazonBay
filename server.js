57c2b4a7cf3eb82996eed2c319b1c4f06cea7577
Kelly Miller <kellyannemiller@gmail.com>
2017-07-27 10:54:53 -0600

    add get store to server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const database = require('./db/knex')
 const bodyParser = require('body-parser');
 
 app.use(express.static(`${__dirname}/public`));
@@ -11,6 +11,16 @@ app.set('port', process.env.PORT || 3000);
 app.get('/', (request, response) => {
   response.sendFile('index.html');
 });
+
+app.get('/api/v1/store', (req, res) => {
+  database('store').select()
+  .then(items => {
+    res.status(200).json(items);
+  })
+  .catch(error => {
+    res.status(500).json({error})
+  })
+})
 
 app.listen(app.get('port'), () => {
   console.log(`YOU DID IT! Server is running on localhost:${app.get('port')}`);
